8455a8a82b6e42c76d24a54d0a251ef6de33fefc
nenadradojcic <nenadradojcic@yahoo.com>
2019-08-01 10:36:03 +0200

    Update TopDownRpgQuestLog.cs

--- Assets/Top Down Character Controller/Scripts/Questing/TopDownRpgQuestLog.cs
+++ Assets/Top Down Character Controller/Scripts/Questing/TopDownRpgQuestLog.cs
@@ -14,40 +14,59 @@ public class TopDownRpgQuestLog : MonoBehaviour {
     public List<TopDownRpgQuest> activeQuests;
     public List<TopDownRpgQuest> instantiatedQuestButtons;
 
+    private void Awake() {
+        questDescriptionText.text = string.Empty;
+    }
+
     public void UpdateQuestList() {
+
+        foreach (Transform go in questButtonHolder.transform) {
+            Destroy(go.gameObject);
+            print("Destroying all children in questButtonHolder.");
+        }
+
+        instantiatedQuestButtons.Clear();
+        print("Cleard instantiatedQuestButtons List");
+
         if (TopDownRpgQuestHub.instance.startedQuests.Count > 0) {
 
-            if (instantiatedQuestButtons.Count > 0) {
-                foreach (Transform go in questButtonHolder.transform) {
-                    Destroy(go.gameObject);
-                }
+            print("There are more than 0 started quests.");
 
-                instantiatedQuestButtons.Clear();
-            }
-            
             activeQuests = TopDownRpgQuestHub.instance.startedQuests;
-            print(activeQuests.Count.ToString());
+            print("We got quests to activeQuests.");
 
-            if (activeQuests.Count > 0) {
+            if (activeQuests.Count > 0) { 
                 for (int q = 0; q < activeQuests.Count; q++) {
-                    if (instantiatedQuestButtons.Count == 0) {
-                        GameObject questButton = Instantiate(questButtonPrefab);
-                        questButton.transform.SetParent(questButtonHolder.transform, false);
-                        questButton.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+                    if (activeQuests[q].questFinished == false) { //This we need so we are able to clear instantiated ui button from quest button holder
+                        if (instantiatedQuestButtons.Count == 0) {
+                            GameObject questButton = Instantiate(questButtonPrefab);
+                            questButton.transform.SetParent(questButtonHolder.transform, false);
+                            questButton.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
 
-                        questButton.GetComponent<TopDownRpgQuestUIHolder>().questSlotted = activeQuests[q];
-                        questButton.GetComponent<TopDownRpgQuestUIHolder>().questDescription = questDescriptionText;
+                            questButton.GetComponent<TopDownRpgQuestUIHolder>().questNameText.text = activeQuests[q].questName;
+                            questButton.GetComponent<TopDownRpgQuestUIHolder>().questSlotted = activeQuests[q];
+                            questButton.GetComponent<TopDownRpgQuestUIHolder>().questDescription = questDescriptionText;
+
+                        if(questDescriptionText.text == string.Empty) {
+                            questDescriptionText.text = activeQuests[q].questDescription;
+                        }
+
+                        print("Added quest `" + activeQuests[q].questName + "` to log as first one.");
+                        }
+                        else {
+                            GameObject questButton = Instantiate(questButtonPrefab);
+                            questButton.transform.SetParent(questButtonHolder.transform, false);
+                            questButton.GetComponent<RectTransform>().anchoredPosition = new Vector2(0f, -(instantiatedQuestButtons.Count * 14f));
+
+                            questButton.GetComponent<TopDownRpgQuestUIHolder>().questSlotted = activeQuests[q];
+                            questButton.GetComponent<TopDownRpgQuestUIHolder>().questDescription = questDescriptionText;
+
+                            print("Added quest `" + activeQuests[q].questName + "` to log as next one.");
+                        }
+
+                        instantiatedQuestButtons.Add(activeQuests[q]);
+                        print("Aded physical quest button to log for quest `" + activeQuests[q].questName + "`");
                     }
-                    else {
-                        GameObject questButton = Instantiate(questButtonPrefab);
-                        questButton.transform.SetParent(questButtonHolder.transform, false);
-                        questButton.GetComponent<RectTransform>().anchoredPosition = new Vector2(0f, -(instantiatedQuestButtons.Count * 14f));
-
-                        questButton.GetComponent<TopDownRpgQuestUIHolder>().questSlotted = activeQuests[q];
-                        questButton.GetComponent<TopDownRpgQuestUIHolder>().questDescription = questDescriptionText;
-                    }
-
-                    instantiatedQuestButtons.Add(activeQuests[q]);
                 }
             }
         }
